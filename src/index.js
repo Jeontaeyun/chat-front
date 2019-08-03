8f82965ef85fccb25b929f6ee297f71a96684e54
Jeontaeyun <https://github.com/Jeontaeyun>
2019-08-03 14:00:33 +1000

    01. webpack 설정 빼오기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -9,4 +9,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Service Worker를 사용하지 않을 것이기 때문에 꺼준다. 
+// serviceWorker.unregister();
